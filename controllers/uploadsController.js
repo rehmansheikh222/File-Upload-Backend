4b75876fee8371d1e9f8a8342f405cd26aeb64ae
unknown <sh.rehman222@gmail.com>
2023-03-06 18:56:14 +0500

    Express Fileupload

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,9 +1,19 @@
-const Product = require('../models/Product')
+const path = require('path')
+
 const { StatusCodes } = require('http-status-codes')
 
 const uploadProductImage = async (req, res) => {
-  console.log(req)
-  res.send('upload product image')
+  const productImage = req.files.image
+
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  )
+  await productImage.mv(imagePath)
+
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } })
 }
 
 module.exports = {
